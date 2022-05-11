eed7e2298972f951297c148da2af76e3f2fbbb33
Rick Janssen <I454010@fhict.nl>
2022-05-11 23:00:38 +0200

    fixed Null error

--- DAL/Models/Product.cs
+++ DAL/Models/Product.cs
@@ -5,9 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int Temp { get; set; }
-        public string EAN { get; set; }
+        public string? EAN { get; set; }
         public float Price { get; set; }
-        public Category category { get; set; }
+        public Category? category { get; set; }
         public int Stock { get; set; }
     }
 }
